dfba3f8589e00e2c4342b16eb1a9834c3a3f9cab
Abdi Ahmed <89213120+abdirashidexe@users.noreply.github.com>
2025-11-04 14:05:17 -0800

    placeholder

--- src/components/components2/Portfolio.jsx
+++ src/components/components2/Portfolio.jsx
@@ -1,5 +1,6 @@
 import eidFlyer from "./../../assets/eid_flyer.png"
 import yoloFlyer from "./../../assets/yolo_flyer.png"
+import placeholder from "./../../assets/placeholder.png"
 
 export default function Portfolio() {
   return (
@@ -11,32 +12,32 @@ export default function Portfolio() {
         <div className="portfolio-grid">
           <img src={eidFlyer} alt="Eid Flyer" />
           <img src={yoloFlyer} alt="Youth Event" />
-          {/* <img src="public/images/flyer3.jpg" alt="YMCA Flyer" />
-          <img src="public/images/flyer4.jpg" alt="Hospital Flyer" /> */}
+          <img src={placeholder} alt="YMCA Flyer" />
+          <img src={placeholder} alt="Hospital Flyer" />
         </div>
       </div>
 
       <div className="portfolio-group">
         <h3>Social Media Graphics</h3>
         <div className="portfolio-grid">
-          <img src="public/images/social1.jpg" alt="Social 1" />
-          <img src="public/images/social2.jpg" alt="Social 2" />
+          <img src={placeholder} alt="Social 1" />
+          <img src={placeholder} alt="Social 2" />
         </div>
       </div>
 
       <div className="portfolio-group">
         <h3>Small Business Ads</h3>
         <div className="portfolio-grid">
-          <img src="public/images/ad1.jpg" alt="Bakery Ad" />
-          <img src="public/images/ad2.jpg" alt="Cafe Ad" />
+          <img src={placeholder} alt="Car Detailing Ad" />
+          <img src={placeholder}  alt="Bakery/Cafe Ad" />
         </div>
       </div>
 
       <div className="portfolio-group">
         <h3>Thumbnails ($5–$15)</h3>
         <div className="portfolio-grid">
-          <img src="public/images/thumb1.jpg" alt="Omar Suleiman" />
-          <img src="public/images/thumb2.jpg" alt="Islamic Reminder" />
+          <img src={placeholder} alt="Omar Suleiman" />
+          <img src={placeholder} alt="Islamic Reminder" />
         </div>
       </div>
     </section>
