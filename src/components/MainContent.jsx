7cdcfa2656dd41a285a74b04084a76b07456a6bb
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2025-11-03 21:12:59 -0800

    update favicon

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,10 +1,12 @@
+import flowerGif from './../assets/flowers.gif' 
+
 export default function MainContent() {
     return(
         <>
             <div id="main-div">
-                <img src="public/images/flowers.gif" alt="handpicking flowers" id="flowersGif"></img>
+                <img src={flowerGif} alt="handpicking flowers" id="flowersGif"></img>
                 <p id="coming-text">coming soon.</p>
-                <a href="https://www.instagram.com/itsbyihsan/" target="blank">follow for updates.</a>
+                <a href="https://www.instagram.com/itsbyihsan/" target="blank" id="ig-link">follow for updates.</a>
             </div>
         </>
     )
