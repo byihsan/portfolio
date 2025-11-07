6f8c0443c4e671f7a7883f56faad7a55e7e8ec0a
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2025-11-07 12:07:32 -0800

    to blue

--- src/components/components2/Hero.jsx
+++ src/components/components2/Hero.jsx
@@ -10,7 +10,7 @@ export default function Hero() {
       <section id="hero">
         <h1>Hi, I’m Ihsan.</h1>
         <p className="subtext">Graphic Designer | 8 Years Experience</p>
-        {/* <h2>I offer your first design FREE!</h2> */}
+        <h2 id="charity-note">10% of profits go to charity.</h2>
         <a href="#contact">
           <button className="primary-btn">Order Today</button>
         </a>
